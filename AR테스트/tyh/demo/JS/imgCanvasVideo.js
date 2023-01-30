6c26a467f920b6d0ebebaf39bee37d50274f6d54
Takyunho <tyh1819@gmail.com>
2023-01-30 17:02:50 +0900

    feat: hls.js 라이브러리를 이용하여 m3u8 파일 스트리밍 하기

--- AR테스트/tyh/demo/JS/imgCanvasVideo.js
+++ AR테스트/tyh/demo/JS/imgCanvasVideo.js
@@ -1,5 +1,14 @@
-  const video = document.getElementById('video');
+  // yolo rtsp
+const video = document.getElementById('video');
   const videoSrc = 'http://183.111.79.81:8000/rtsp/cam1/index.m3u8';
   const hls = new Hls();
   hls.loadSource(videoSrc);
   hls.attachMedia(video);
+
+
+  // 일반 rtsp
+  var video_rtsp = document.getElementById('video-rtsp2');
+	var videoSrc_rtsp = 'https://ar.idb.ai:8443/rtsp/cam1/index.m3u8';
+	var hls2 = new Hls();
+	hls2.loadSource(videoSrc_rtsp);
+	hls2.attachMedia(video_rtsp);
